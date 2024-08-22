79032eb7fea1577bedc826c0448f0d79fda55a27
kevalbarvaliya7114 <sq.keval@gmail.com>
2024-08-22 11:16:52 +0530

    22-08-2024

---
+++ 5Math.js
@@ -0,0 +1,46 @@
+// Math object Constant
+
+// console.log(Math.E);
+// console.log(Math.PI);
+// console.log(Math.SQRT2);
+// console.log(Math.SQRT1_2);
+// console.log(Math.LN10);
+// console.log(Math.LN2);
+// console.log(Math.LOG10E);
+// console.log(Math.LOG2E);
+
+// Method
+
+console.log(Math.ceil(2.1));
+console.log(Math.ceil(-2.9));
+console.log(Math.floor(2.1));
+console.log(Math.floor(-2.9));
+console.log(Math.trunc(2.1));
+console.log(Math.trunc(-2.9));
+console.log(Math.round(2.1));
+console.log(Math.round(-2.9));
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.sqrt(4));
+console.log(Math.sqrt(15));
+console.log(Math.pow(4,2));
+console.log(Math.sign(15));
+console.log(Math.sign(-15));
+console.log(Math.sign(0));
+console.log(Math.abs(-15));
+console.log(Math.abs(10));
+console.log(Math.sin(90*Math.PI/180));
+console.log(Math.sin(0*Math.PI/180));
+console.log(Math.cos(90*Math.PI/180));
+console.log(Math.cos(0*Math.PI/180));
+console.log(Math.tan(90*Math.PI/180));
+console.log(Math.tan(0*Math.PI/180));
+console.log(Math.log(10));
+console.log(Math.log2(8));
+console.log(Math.log10(105));
+
+console.log(Math.max(50,60,87,60,40,44,47));
+console.log(Math.min(50,60,87,60,40,44,47));
+
+let a=Math.floor((Math.random()*10)+1);
+console.log(a);
