1ec255f51227e4cd7d92420b5cdbd257f4d599e0
kevalbarvaliya7114 <sq.keval@gmail.com>
2024-09-02 11:33:24 +0530

    02-09-2024

--- 8Array.js
+++ 8Array.js
@@ -42,19 +42,61 @@
 // console.log(data.concat(["Hello"],a));
 
 // Sort() -> sorting elements 
-let data = ['z','y','s','t','a','c'];
-console.log(data.sort());
-console.log(data.sort((a,b)=>a-b));
+// let data = ['z','y','s','t','a','c'];
+// console.log(data.sort());
+// console.log(data.sort((a,b)=>a-b));
 
 // Reverse() -> reverse element
-console.log(data.reverse());
+// console.log(data.reverse());
 
 // includes() -> check element includes or not
-console.log(data.includes('a'));
+// console.log(data.includes('a'));
 
 // Slice -> Slice to array
-console.log(data.slice(1,3));4
+// console.log(data.slice(1,3));4
 
 // splice(startIndex, deletecount) -> removing specific index element
-console.log(data.splice(3,3,500,546,854));
+// console.log(data.splice(3,3,500,546,854));
+// console.log(data);
+
+// Array.isArray() -> check Array or not ?
+console.log(Array.isArray([323]));
+
+// Max & Min
+// let data = [22,55,99,230,40,60,75,65];
+// console.log(data);
+function maxFun(arr) {
+    return Math.max.apply(null, arr);
+}
+function minFun(arr) {
+    return Math.min.apply(null, arr);
+}
+// console.log(maxFun(data));
+// console.log(minFun(data));
+
+// join() -> Join with seperator(, - / (space) ? )
+// console.log(data.join("-"));
+
+// flat() -> remove inner array and convert into one array
+let abc = [45, 57, [52, 45], 50, [87, [59, 78]]];
+console.log(abc.flat().flat());
+
+
+// HIGHERORDER METHOD (CALL FUNCTION)
+
+let data = [22, 55, 99, 230, 40, 60, 75, 65];
 console.log(data);
+
+// filter() -> FILTER DATA
+function hii(value, index, array) {
+    return value < 35;
+}
+let ihh = data.filter(hii);
+console.log(ihh);
+
+// some() -> check base on condition
+function ohh(value, index, array) {
+    return value === 55;
+}
+let aa = data.some(ohh);
+console.log(aa);
---
+++ MapMethods.js
@@ -0,0 +1,38 @@
+// Map Methods
+
+// let data = new Map([
+//     [1,'hello']
+// ]);
+// data.set(2,'ye');
+// data.set(30,'yue');
+// data.set(4,'ohk');
+// console.log(data);
+// console.log(data.get(2));
+// console.log(data.delete(2));
+// console.log(data.has(4));
+// console.log(data.size);
+// console.log(data.entries());
+
+// let txt = "";
+// data.forEach((val,key) =>{
+//     txt += val + "~~~~~" + key + "\n"
+// })
+// console.log(txt);
+
+// Set Methods 
+
+// let data = new Set([1,2,3,4,5]);
+// console.log(data);
+// data.add(10);
+// data.delete(1);
+// data.entries();
+// console.log(data.has(5));
+// console.log(data.keys());
+// console.log(data.size);
+// console.log(data.values());
+
+// let txt = "";
+// data.forEach((val,key) =>{
+//     txt += val + "~~~~~" + key + "\n"
+// })
+// console.log(txt);
